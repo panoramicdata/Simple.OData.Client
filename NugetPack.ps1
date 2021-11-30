67d6c15d3daffba804c63d67f51ae6844f06cd94
David Bond <david.bond@panoramicdata.com>
2021-11-30 10:38:00 +0000

    Fixed nugetpack

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,3 +1,3 @@
-nuget pack ./PanoramicData.OData.Client.nuspe -Symbols -Version $args[0] -OutputDirectory build/packages -SymbolPackageFormat snupkg
+nuget pack ./PanoramicData.OData.Client.nuspec -Symbols -Version $args[0] -OutputDirectory build/packages -SymbolPackageFormat snupkg
 nuget pack ./PanoramicData.OData.V3.Client.nuspec -Symbols -Version $args[0] -OutputDirectory build/packages -SymbolPackageFormat snupkg
 nuget pack ./PanoramicData.OData.V4.Client.nuspec -Symbols -Version $args[0] -OutputDirectory build/packages -SymbolPackageFormat snupkg
